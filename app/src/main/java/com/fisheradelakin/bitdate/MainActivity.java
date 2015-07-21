7bd1b4ca4bb869a251f6f926f6957674fb3348f3
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-07-21 16:30:01 -0700

    create parse user with facebook

--- app/src/main/java/com/fisheradelakin/bitdate/MainActivity.java
+++ app/src/main/java/com/fisheradelakin/bitdate/MainActivity.java
@@ -1,9 +1,12 @@
 package com.fisheradelakin.bitdate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(ParseUser.getCurrentUser() == null) {
+            Intent i = new Intent(this, SignInActivity.class);
+            startActivity(i);
+        }
     }
 
 
