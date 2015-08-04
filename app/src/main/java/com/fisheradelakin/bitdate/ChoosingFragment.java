866093d8b71db1cffd3a64a469fec40e9894c393
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-08-03 17:00:22 -0700

    connect the adapter

--- app/src/main/java/com/fisheradelakin/bitdate/ChoosingFragment.java
+++ app/src/main/java/com/fisheradelakin/bitdate/ChoosingFragment.java
@@ -8,6 +8,9 @@ import android.view.ViewGroup;
 
 import com.parse.ParseObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -21,6 +24,13 @@ public class ChoosingFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
 
+        CardStackContainer cardStack = (CardStackContainer) v.findViewById(R.id.card_stack);
+        User user = new User();
+        user.setFirstName("Fisher");
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        CardAdapter cardAdapter = new CardAdapter(getActivity(), users);
+        cardStack.setCardAdapter(cardAdapter);
         return v;
     }
 }
