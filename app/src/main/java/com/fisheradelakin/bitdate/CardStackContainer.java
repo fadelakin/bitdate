9b07a8d21eb10c8fd7cbab528528e41076f6782d
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-08-03 17:16:09 -0700

    moving the card

--- app/src/main/java/com/fisheradelakin/bitdate/CardStackContainer.java
+++ app/src/main/java/com/fisheradelakin/bitdate/CardStackContainer.java
@@ -3,12 +3,22 @@ package com.fisheradelakin.bitdate;
 import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 /**
  * Created by Fisher on 8/3/15.
  */
-public class CardStackContainer extends RelativeLayout {
+public class CardStackContainer extends RelativeLayout implements View.OnTouchListener {
+
+    private static final String TAG = "CardStackContainer";
+
+    private float mLastTouchX;
+    private float mLastTouchY;
+    private float mPositionX;
+    private float mPositionY;
 
     private CardAdapter mCardAdapter;
 
@@ -29,7 +39,40 @@ public class CardStackContainer extends RelativeLayout {
 
         if(mCardAdapter.getCount() > 0) {
             CardView cardView = mCardAdapter.getView(0, null, this);
+            cardView.setOnTouchListener(this);
             addView(cardView);
         }
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                mLastTouchX = event.getX();
+                mLastTouchY = event.getY();
+
+                mPositionX = v.getX();
+                mPositionY = v.getY();
+
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float xMove = event.getX();
+                float yMove = event.getY();
+
+                float changeX = xMove - mLastTouchX;
+                float changeY = yMove - mLastTouchY;
+
+                mPositionX += changeX;
+                mPositionY += changeY;
+
+                v.setX(mPositionX);
+                v.setY(mPositionY);
+
+                break;
+        }
+        return true;
     }
 }
